6a4ab2e59583c988e01c3165a4e21b5fe26267b3
Lauren Tan <lauren.elizabeth.tan@gmail.com>
2015-09-28 15:08:14 -0400

    Initial work

---
+++ addon/index.js
@@ -0,0 +1,1 @@
+export { default } from 'ember-macaroni/utils/macros';
--- addon/utils/macros.js
+++ addon/utils/macros.js
@@ -1,45 +1,17 @@
-# ember-macaroni [![Build Status](https://travis-ci.org/poteto/ember-macaroni.svg?branch=master)](https://travis-ci.org/poteto/ember-macaroni)
+import Ember from 'ember';
 
-Delicious computed property macaronis (macros) for Ember.js 1.13x and greater.
+const {
+  computed,
+  get,
+  getProperties,
+  getWithDefault,
+  isPresent,
+  A: emberArray
+} = Ember;
 
-![](http://i.imgur.com/XV4VFJl.jpg)
-
-## Usage
-First, import the macro(s):
-
-```js
-import macros from 'ember-macaroni'; // imports all the things
-import { findFromCollectionByKey } from 'ember-macaroni'; // imports a named macro
-
-export default Ember.Component.extend({
-  items: null,
-  selectedId: null,
-  selectedItem: findFromCollectionByKey('items', 'id', 'selectedId'),
-
-  init() {
-    this.items = [
-      { id: 1, name: 'Derek Zoolander' },
-      { id: 2, name: 'Hansel' },
-      { id: 3, name: 'Mugatu' }
-    ];
-  },
-
-  actions: {
-    selectPerson(id) {
-      Ember.set(this, 'selectedId', id);
-    }
-  }
-});
-```
-
-## Macro API
-
-#### `findFromCollectionByKey(collectionKey, propName, valueKey)`
-
-```js
-/** 
+/**
  * Returns the first item with a property matching the passed value from a dependent key.
- * 
+ *
  * Ember.Object.extend({
  *   items: [{ id: 1, name: 'foo' }, { id: 2, name: 'bar' }],
  *   selectedId: 1,
@@ -50,12 +22,16 @@ export default Ember.Component.extend({
  * @param {String} propName The key name for the property to find by
  * @param {String} valueKey The key name that returns the value to find
 */
-```
+function findFromCollectionByKey(collectionKey, propName, valueKey) {
+  return computed(`${collectionKey}.@each.${propName}`, valueKey, {
+    get() {
+      return emberArray(get(this, collectionKey))
+        .findBy(propName, get(this, valueKey));
+    }
+  });
+}
 
-#### `findFromCollectionByValue(collectionKey, propName, value)`
-
-```js
-/** 
+/**
  * Returns the first item with a property matching the passed value.
  *
  * Ember.Object.extend({
@@ -67,14 +43,18 @@ export default Ember.Component.extend({
  * @param {String} propName The key name for the property to find by
  * @param {*} value The value to match
 */
-```
+function findFromCollectionByValue(collectionKey, propName, value) {
+  return computed(`${collectionKey}.@each.${propName}`, {
+    get() {
+      return emberArray(get(this, collectionKey))
+        .findBy(propName, value);
+    }
+  });
+}
 
-#### `rejectFromCollectionByKey(collectionKey, propName, valueKey)`
-
-```js
-/** 
+/**
  * Returns an array with the items that do not match the passed value from a dependent key.
- * 
+ *
  * Ember.Object.extend({
  *   items: [{ id: 1, name: 'foo' }, { id: 2, name: 'bar' }],
  *   selectedId: 2,
@@ -85,14 +65,18 @@ export default Ember.Component.extend({
  * @param {String} propName The key name for the property to reject by
  * @param {String} valueKey The key name that returns the value to reject
 */
-```
+function rejectFromCollectionByKey(collectionKey, propName, valueKey) {
+  return computed(`${collectionKey}.@each.${propName}`, valueKey, {
+    get() {
+      return emberArray(get(this, collectionKey))
+        .rejectBy(propName, get(this, valueKey));
+    }
+  });
+}
 
-#### `rejectFromCollectionByValue(collectionKey, propName, value)`
-
-```js
-/** 
+/**
  * Returns an array with the items that do not match the passed value.
- * 
+ *
  * Ember.Object.extend({
  *   items: [{ id: 1, name: 'foo' }, { id: 2, name: 'bar' }],
  *   selectedItem: rejectFromCollectionByValue('items', 'id', 2) // [{ id: 1, name: 'foo' }]
@@ -102,12 +86,16 @@ export default Ember.Component.extend({
  * @param {String} propName The key name for the property to reject by
  * @param {*} value The value to reject
 */
-```
+function rejectFromCollectionByValue(collectionKey, propName, value) {
+  return computed(`${collectionKey}.@each.${propName}`, {
+    get() {
+      return emberArray(get(this, collectionKey))
+        .rejectBy(propName, value);
+    }
+  });
+}
 
-#### `filterFromCollectionByKey(collectionKey, propName, valueKey)`
-
-```js
-/** 
+/**
  * Returns an array with just the items with the matched property.
  *
  * Ember.Object.extend({
@@ -115,135 +103,162 @@ export default Ember.Component.extend({
  *   selectedId: 1,
  *   selectedItem: filterFromCollectionByKey('items', 'id', 'selectedId') // [{ id: 1, name: 'foo' }]
  * });
- * 
+ *
  * @param {String} collectionKey The key name for the collection
  * @param {String} propName The key name for the property to filter by
  * @param {String} valueKey The key name that returns the value to filter
 */
-```
+function filterFromCollectionByKey(collectionKey, propName, valueKey) {
+  return computed(`${collectionKey}.@each.${propName}`, valueKey, {
+    get() {
+      return emberArray(get(this, collectionKey))
+        .filterBy(propName, get(this, valueKey));
+    }
+  });
+}
 
-#### `filterCollectionByContains(collectionKey, propName, values)`
-
-```js
-/** 
+/**
  * Returns an array with just the items that are contained in another array.
- * 
+ *
  * Ember.Object.extend({
  *   items: [{ id: 1, name: 'foo' }, { id: 2, name: 'bar' }],
  *   selectedId: 1,
  *   selectedItem: filterCollectionByContains('items', 'id', [1]) // [{ id: 1, name: 'foo' }]
  * });
- * 
+ *
  * @param {String} collectionKey The key name for the collection
  * @param {String} propName The key name for the property to filter by
  * @param {Array} values The array of values to filter
 */
-```
+function filterCollectionByContains(collectionKey, propName, values = []) {
+  return computed(`${collectionKey}.@each.${propName}`, {
+    get() {
+      return emberArray(get(this, collectionKey))
+        .filter((item) => emberArray(values).contains(get(item, propName)));
+    }
+  });
+}
 
-#### `reduceCollectionByKey(collectionKey, dependentKey, startValue)`
-
-```js
-/** 
+/**
  * Combines the values of the enumerator into a single value, using a dependent key.
- * 
+ *
  * Ember.Object.extend({
  *   items: [{ name: 'foo', age: 2 }, { name: 'bar', age: 5 }],
  *   selectedItem: reduceCollectionByKey('items', 'age', 0) // 7
  * });
- * 
+ *
  * @param {String} collectionKey The key name for the collection
  * @param {String} dependentKey The key name for the property to reduce
  * @param {*} startValue The initial value
 */
-```
+function reduceCollectionByKey(collectionKey, dependentKey, startValue = 0) {
+  return computed(`${collectionKey}.@each.${dependentKey}`, {
+    get() {
+      return emberArray(get(this, collectionKey))
+        .reduce((total, current) => total + parseFloat(getWithDefault(current, dependentKey, 0)), startValue);
+    }
+  });
+}
 
-#### `isEqualByKeys(firstKey, secondKey)`
-
-```js
-/** 
+/**
  * Strict equality using dependent keys.
- * 
+ *
  * Ember.Object.extend({
  *   employeeId: 1
  *   selectedId: 1,
  *   isSelected: isEqualByKeys('employeeId', 'selectedId') // true
  * });
- * 
+ *
  * @param {String} firstKey The key name for the first property
  * @param {String} secondKey The key name for the second property
 */
-```
+function isEqualByKeys(firstKey, secondKey) {
+  return computed(firstKey, secondKey, {
+    get() {
+      return get(this, firstKey) === get(this, secondKey);
+    }
+  });
+}
 
-#### `getPropertiesByKeys(...dependentKeys)`
-
-```js
-/** 
+/**
  * Returns a POJO containing all the key-values that match the dependent keys.
- * 
+ *
  * Ember.Object.extend({
  *   age: 5,
  *   name: 'foo',
  *   props: getPropertiesByKeys('age', 'name') // { age: 5, name: 'foo' }
  * });
- * 
+ *
  * @param {...rest} dependentKeys Argument list of dependent keys
 */
-```
+function getPropertiesByKeys(...dependentKeys) {
+  const computedFunc = computed({
+    get() {
+      return getProperties(this, dependentKeys);
+    }
+  });
 
-#### `ifThenElseWithKeys(conditionalKey, trueKey, falseKey)`
+  return computedFunc.property.apply(computedFunc, dependentKeys);
+}
 
-```js
-/** 
+/**
  * Ternary conditional with dependent keys.
- * 
+ *
  * Ember.Object.extend({
  *   isSelected: true,
  *   selectedText: 'Is Enabled',
 *    deselectedText: 'Is Disabled',
  *   displayText: ifThenElseWithKeys('isSelected', 'selectedText', 'deselectedText') // 'Is Enabled'
  * });
- * 
+ *
  * @param {String} conditionalKey The key name for the conditional property
  * @param {String} trueKey The key name for the property to return when the conditional is true
  * @param {String} falseKey The key name for the property to return when the conditional is false
 */
-```
+function ifThenElseWithKeys(conditionalKey, trueKey, falseKey) {
+  return computed(conditionalKey, trueKey, falseKey, {
+    get() {
+      const conditionalValue = get(this, conditionalKey);
+      const trueValue = get(this, trueKey);
+      const falseValue = get(this, falseKey);
 
-#### `ifThenElseWithValues(conditionalKey, trueValue, falseValue)`
+      return (isPresent(conditionalValue) && !!conditionalValue) ? trueValue : falseValue;
+    }
+  });
+}
 
-```js
-/** 
+/**
  * Ternary conditional.
- * 
+ *
  * Ember.Object.extend({
  *   isSelected: true,
  *   displayText: ifThenElseWithKeys('isSelected', 'Is Enabled', 'Is Disabled') // 'Is Enabled'
  * });
- * 
+ *
  * @param {String} conditionalKey The key name for the conditional property
  * @param {String} trueValue The value to return when the conditional is true
  * @param {String} falseValue The value to return when the conditional is false
 */
-```
+function ifThenElseWithValues(conditionalKey, trueValue = true, falseValue = false) {
+  return computed(conditionalKey, {
+    get() {
+      const conditionalValue = get(this, conditionalKey);
 
-## Installation
+      return (isPresent(conditionalValue) && !!conditionalValue) ? trueValue : falseValue;
+    }
+  });
+}
 
-* `git clone` this repository
-* `npm install`
-* `bower install`
-
-## Running
-
-* `ember server`
-* Visit your app at http://localhost:4200.
-
-## Running Tests
-
-* `ember test`
-* `ember test --server`
-
-## Building
-
-* `ember build`
-
-For more information on using ember-cli, visit [http://www.ember-cli.com/](http://www.ember-cli.com/).
+export default {
+  findFromCollectionByKey,
+  findFromCollectionByValue,
+  rejectFromCollectionByKey,
+  rejectFromCollectionByValue,
+  filterFromCollectionByKey,
+  filterCollectionByContains,
+  reduceCollectionByKey,
+  isEqualByKeys,
+  getPropertiesByKeys,
+  ifThenElseWithKeys,
+  ifThenElseWithValues
+};
